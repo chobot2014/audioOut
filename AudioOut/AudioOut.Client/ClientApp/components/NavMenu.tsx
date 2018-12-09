b698cee22cdd40b8670ae2045bb77a34892d4a09
John Chohon <johnchohon@gmail.com>
2018-12-09 16:52:10 -0600

    initial client upload

---
+++ AudioOut/AudioOut.Client/ClientApp/components/NavMenu.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+export class NavMenu extends React.Component<{}, {}> {
+    public render() {
+        return <div className='main-nav'>
+        </div>;
+    }
+}
